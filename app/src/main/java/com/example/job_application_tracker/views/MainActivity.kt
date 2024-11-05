271b4b1c082e7d082fdcac42d305d9e8eb31ba09
goeremie <gabriel.eremie@yahoo.com>
2024-11-05 15:04:09 -0800

    animations

--- app/src/main/java/com/example/job_application_tracker/views/MainActivity.kt
+++ app/src/main/java/com/example/job_application_tracker/views/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.job_application_tracker.views
 
 import android.os.Bundle
 import android.view.View
+import android.view.animation.AnimationUtils
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
@@ -31,7 +32,10 @@ class MainActivity : AppCompatActivity(), BottomAppBarVisibility,FragmentNavigat
         setContentView(binding.root)
         init()
 
+        val shakeAnim = AnimationUtils.loadAnimation(this, R.anim.shake)
+
         binding.fab.setOnClickListener {
+            it.startAnimation(shakeAnim)
             hideBottomAppBar()
             loadFragment(addApplicationFragment)
         }
