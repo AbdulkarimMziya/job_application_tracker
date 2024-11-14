856c793612d2ce16172977e37a985880ae54ec8c
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2024-11-14 11:16:28 -0800

    new feature

---
+++ app/src/main/java/com/example/job_application_tracker/AuthenticateMainFragment.kt
@@ -0,0 +1,21 @@
+package com.example.job_application_tracker
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+
+class AuthenticateMainFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_authenticate_main, container, false)
+    }
+
+}
