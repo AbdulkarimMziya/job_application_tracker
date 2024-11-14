38ec192c03a883b56c136209e53873529eba68f6
AbdulkarimMziya <66888226+AbdulkarimMziya@users.noreply.github.com>
2024-11-14 15:00:12 -0800

    nav graph component setup

--- app/src/main/java/com/example/job_application_tracker/AuthenticateMainFragment.kt
+++ app/src/main/java/com/example/job_application_tracker/AuthenticateMainFragment.kt
@@ -5,17 +5,50 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.navigation.findNavController
+import com.example.job_application_tracker.databinding.ActivityAuthenticationBinding
+import com.example.job_application_tracker.databinding.FragmentAuthenticateMainBinding
 
 
 class AuthenticateMainFragment : Fragment() {
+    private lateinit var authFragBinding: FragmentAuthenticateMainBinding
+
+    private lateinit var btnSignIn: Button
+    private lateinit var btnSignUp: Button
+    private lateinit var btnGoogleLogin: Button
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        authFragBinding = FragmentAuthenticateMainBinding.inflate(layoutInflater,container,false)
+
+        init()
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_authenticate_main, container, false)
+        return authFragBinding.root
     }
+
+    private fun init() {
+        btnSignIn = authFragBinding.btnSignIn
+        btnSignUp = authFragBinding.btnSignUp
+        btnGoogleLogin = authFragBinding.btnGoogleLogin
+
+        navigateComponent()
+    }
+
+    private fun navigateComponent(){
+        btnSignIn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_authenticateMainFragment_to_signInFragment)
+        }
+
+        btnSignUp.setOnClickListener {
+            it.findNavController().navigate(R.id.action_authenticateMainFragment_to_signupFragment)
+        }
+    }
+
 
 }
--- app/src/main/java/com/example/job_application_tracker/AuthenticationActivity.kt
+++ app/src/main/java/com/example/job_application_tracker/AuthenticationActivity.kt
@@ -5,12 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
 
 class AuthenticationActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         setContentView(R.layout.activity_authentication)
 
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        navController =   findNavController(R.id.nav_host_fragment)
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
